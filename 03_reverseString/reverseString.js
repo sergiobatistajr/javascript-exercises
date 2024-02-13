7cb5fb1c32bdfd9206e05fd61b988a9a7d91fc78
Sergio Batista <sergiobatistajr94@gmail.com>
2024-02-13 10:51:41 -0300

    Lazy commit ex

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+  let reversedWord = "";
+  for (let i = word.length - 1; i >= 0; i--) {
+    reversedWord += word[i];
+  }
+  return reversedWord;
 };
 
 // Do not edit below this line
