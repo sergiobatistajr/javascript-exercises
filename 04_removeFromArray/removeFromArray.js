4e08aed0cf6884f823ac7490c367c13c1c5c5bf3
Sergio Batista <sergiobatistajr94@gmail.com>
2024-02-13 11:11:30 -0300

    Lazy commit ex 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+  let result = [];
 
+  for (let i = 0; i < arr.length; i++) {
+    let shouldExclude = false;
+
+    for (let j = 0; j < args.length; j++) {
+      if (arr[i] === args[j]) {
+        shouldExclude = true;
+        break;
+      }
+    }
+    if (!shouldExclude) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
