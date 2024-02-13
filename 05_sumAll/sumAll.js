3ebb6bebda61f205c05b68f058ccd2a898b6a9da
Sergio Batista <sergiobatistajr94@gmail.com>
2024-02-13 11:43:45 -0300

    Lazy commit ex 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+  let result = 0;
+  if (min > max) {
+    const temp = min;
+    min = max;
+    max = temp;
+  }
+  for (let i = min; i <= max; i++) {
+    result += i;
+  }
+  return result;
 };
 
 // Do not edit below this line
